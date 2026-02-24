575090ad1757096001a53fe203fc73459a3ca756
sumaiya7-ops <sumaiya7-ops@gmail.com>
2026-02-24 06:06:34 -0800

    incomplete js

--- script/pain.js
+++ script/pain.js
@@ -8,3 +8,57 @@ function updateCounts(totalDiff, interviewDiff, rejectedDiff, magicDiff) {
     if (rejectedEl) rejectedEl.innerText = Math.max(0, (parseInt(rejectedEl.innerText) || 0) + rejectedDiff);
     if (magicEl) magicEl.innerText = Math.max(0, (parseInt(magicEl.innerText) || 0) + magicDiff);
 }
+document.addEventListener('click', function (event) {
+    const card = event.target.closest('.job-card');
+    if (!card) return;
+
+    let currentStatus = card.dataset.status || "none";
+    const statusLabel = card.querySelector('.applied');
+    if (event.target.classList.contains('interview-btn')) {
+        if (currentStatus === "interview") return;
+
+        if (currentStatus === "none") {        
+            updateCounts(-1, 1, 0, -1);
+        } else if (currentStatus === "rejected") {     
+            updateCounts(0, 1, -1, 0);
+        }
+
+        card.dataset.status = "interview";
+        if (statusLabel) {
+            statusLabel.innerText = "APPLIED";
+            statusLabel.classList.remove('bg-indigo-100', 'text-indigo-700', 'bg-red-700', 'text-white');
+            statusLabel.classList.add('bg-green-700', 'text-white');
+        }
+    }
+
+   
+    if (event.target.classList.contains('rejected-btn')) {
+        if (currentStatus === "rejected") return;
+
+        if (currentStatus === "none") {           
+            updateCounts(-1, 0, 1, -1);
+        } else if (currentStatus === "interview") {      
+            updateCounts(0, -1, 1, 0);
+        }
+        card.dataset.status = "rejected";
+        if (statusLabel) {
+            statusLabel.innerText = "NOT APPLIED";
+            statusLabel.classList.remove('bg-indigo-100', 'text-indigo-700', 'bg-green-700', 'text-white');
+            statusLabel.classList.add('bg-red-700', 'text-white');
+        }
+    }
+
+    const deleteBtn = event.target.closest('.delete-card') || event.target.closest('.fa-trash-can');
+    if (deleteBtn) {
+        event.stopImmediatePropagation();
+
+        if (currentStatus === "none") {
+            updateCounts(-1, 0, 0, -1);
+        } else if (currentStatus === "interview") {            
+            updateCounts(0, -1, 0, 0);
+        } else if (currentStatus === "rejected") {
+           updateCounts(0, 0, -1, 0);
+        }
+        card.remove();
+    }
+});
